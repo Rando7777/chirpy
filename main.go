aa3e55e817abc5c737377fc5d91bd81f0168de67
Rando <randopoolak@gmail.com>
2025-10-14 19:59:50 +0300

    feat: init file server

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import(
 
 func main(){
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	srv := http.Server{Handler: mux, Addr: ":8080"}
+	
 	fmt.Println("Starting server at 8080...")
 	srv.ListenAndServe()
 }
+
