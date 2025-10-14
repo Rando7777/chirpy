20a243b3db2f9809896dc20d71bd517136e99da1
Rando <randopoolak@gmail.com>
2025-10-14 21:52:31 +0300

    feat: metrics for /app/ path

--- main.go
+++ main.go
@@ -4,21 +4,34 @@ import(
 	"fmt"
 	"net/http"
 	"log"
+	"sync/atomic"
+	"strconv"
 )
 
-func healthzHandler(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+type apiConfig struct{
+	fileserverHits atomic.Int32
 }
+
+func (a *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		a.fileserverHits.Add(1)	
+		next.ServeHTTP(w, r)
+	})
+}
+
 
 func main(){
 	const root = "."
 	const port = "8080"
+	fileHandler := http.StripPrefix("/app", http.FileServer(http.Dir(root)))
+	
+	apiCfg := &apiConfig{}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(root))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileHandler))
 	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.metricsResetHandler)
 
 	srv := &http.Server{
 		Handler: mux, 
@@ -30,4 +43,33 @@ func main(){
 		log.Fatalf("server stopped: %s\n", err)
 	}
 }
+
+
+func healthzHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (a *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	
+	countStr := strconv.Itoa(int(a.fileserverHits.Load()))
+	w.Write([]byte("Hits: " + countStr))
+}
+
+func (a *apiConfig) metricsResetHandler(w http.ResponseWriter, r *http.Request){
+	a.fileserverHits.Store(0)
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+
+
+
+
+
 
